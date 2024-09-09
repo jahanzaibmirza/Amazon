4a0d00a26db2532ce094f170534b4f7b757f36a5
jahanziab <jahanzaibnadeem45@gmail.com>
2024-09-09 15:40:00 +0500

    adding new fields

--- amazon_script/amazon_script/spiders/amazon_data.py
+++ amazon_script/amazon_script/spiders/amazon_data.py
@@ -1,10 +1,17 @@
 import csv
-
+from datetime import datetime
 import scrapy
-
-
 class AmazonDataSpider(scrapy.Spider):
     name = "amazon_data"
+
+    custom_settings = {'ROBOTSTXT_OBEY': False,
+                       'RETRY_TIMES': 5,
+                       'DOWNLOAD_DELAY': 0.5,
+                       'CONCURRENT_REQUESTS': 1,
+                       'HTTPERROR_ALLOW_ALL': True,
+                       'FEED_URI': f'outputs/amazon_listing_data_{datetime.now().strftime("%d_%b_%Y_%H_%M_%S")}.csv',
+                       'FEED_FORMAT': 'csv',
+                       'FEED_EXPORT_ENCODING': 'utf-8'}
 
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -49,35 +56,40 @@ class AmazonDataSpider(scrapy.Spider):
             data=list(csv.DictReader(file))
             return data
 
-
-
     def start_requests(self):
-        input_file= 'AMZN US_Top_Search_Terms (Aug 2024)'
         input_file= 'keywords'
         input_File_Data=self.read_file(input_file)
-        for each_row in input_File_Data:
+        for each_row in input_File_Data[:]:
             term= each_row.get('Search Term').lower().strip()
             url=f'https://www.amazon.com/s?k={term}'
             yield scrapy.Request(url=url, headers=self.headers, callback=self.parse,)
 
-        # url='https://www.amazon.com/s?k=ipod'
-        # yield scrapy.Request(url=url, headers=self.headers, callback=self.parse,)
 
     def parse(self, response):
         item= dict()
         divs= response.xpath('//div[@data-component-type="s-search-result"]')
+        # divs= response.xpath("//div[contains(@class,'s-result-item')]")
         for each_div in divs[:]:
+            item['type']= each_div.xpath(".//preceding::div//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']/text()").get('').strip()
             item['ProductName'] = each_div.xpath('.//h2//span/text()').get('').strip()
             item['ASIN']= each_div.xpath('.//@data-asin').get('').strip()
-            item['Rating']= each_div.xpath("//span[contains(@aria-label,'stars')]/text()").get('').strip()
-            item['NoOfReviews']= each_div.xpath(".//span[contains(@aria-label,'ratings')]/text()").get('').strip().replace('ratings','')
+            item['Rating']= each_div.xpath(".//span[contains(@aria-label,'stars')]/@aria-label").get('').replace('out of 5 stars','').strip()
+            item['NoOfReviews']= each_div.xpath(".//span[contains(@aria-label,'ratings')]/@aria-label").get('').strip().replace('ratings','')
             url_suffix = each_div.xpath(".//h2//a/@href").get('')
             item['URL'] = f'https://www.amazon.com{url_suffix}' if url_suffix else ''
             item['Price']= each_div.xpath(".//span[@class='a-price']//span[@class='a-offscreen']/text()").get('').strip()
             item['SaveCoupon']= each_div.xpath(".//span[@class='s-coupon-unclipped']/span[1]/text()").get('').strip().replace('Save','').strip()
-
             item['ViewPortWidth']='1291'
             item['ViewPortHeight'] ='826'
+
+            small_business=each_div.xpath(".//img[contains(@src,'https://m.media-amazon.com/images/I/111mHoVK0kL._SS200_.png')]/following-sibling::span[contains(text(),'Small Business')]/text()").get('').strip()
+            item['Small Business']= 'TRUE' if 'Small Business' in small_business else 'FALSE'
+
+            limited_deal= each_div.xpath(".//span[contains(text(),'List: ')]/text()").get('')
+            item['Limited Deal Offer']='TRUE' if limited_deal else 'FALSE'
+
+            best_seller= each_div.xpath(".//span[contains(text(),'Best Seller')]/text()").get('').strip()
+            item['Best Seller']= 'TRUE' if "Best Seller" in best_seller else 'FALSE'
 
             yield item
 
